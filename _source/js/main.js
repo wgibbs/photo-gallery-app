f1231421df0b97debb413661e8808c6131db64b2
Wesley Gibbs <wcgibbsey@gmail.com>
2021-04-21 12:10:20 -0400

    removed photo info positioning

--- _source/js/main.js
+++ _source/js/main.js
@@ -5,47 +5,44 @@ var galleryApp = new Vue({
       {
         title: 'Arches National Park, UT',
         image_url: './img/arches-utah-1.jpg',
-        info_position: 'bot-right',
       },
       {
         title: 'Squamish, BC',
         image_url: './img/squamish-bc-2.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Bonneville Salt Flats, UT',
         image_url: './img/bonneville-utah-1.jpg',
-        info_position: 'bot-right',
       },
       {
         title: 'Garden of the Gods - Colorado Springs, CO',
         image_url: './img/garden-of-the-gods-co-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Seattle, WA',
         image_url: './img/seattle-wa-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Eugene, OR',
         image_url: './img/eugene-oregon-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Cumberland Trail - Chattanooga, TN',
         image_url: './img/chattanooga-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Arabia Mountain, GA',
         image_url: './img/arabia-mt-1.jpg',
-        info_position: 'bot-right',
       },
       {
         title: 'San Francisco, CA',
         image_url: './img/san-fran-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Squamish, BC',
@@ -60,7 +57,6 @@ var galleryApp = new Vue({
       {
         title: 'Seattle, WA',
         image_url: './img/seattle-wa-2.jpg',
-        info_position: 'bot-right',
       },
       {
         title: 'Seal Rock, OR',
@@ -70,17 +66,15 @@ var galleryApp = new Vue({
       {
         title: 'Boulder Mountain Park - Boulder, CO',
         image_url: './img/boulder-co-1.jpg',
-        info_position: 'bot-left',
+        info_position: 'top-left',
       },
       {
         title: 'Canby, OR',
         image_url: './img/canby-or-1.jpg',
-        info_position: 'bot-right',
       },
       {
         title: 'Willamette National Forest - Blue River, OR',
         image_url: './img/willamette-national-forest-1.jpg',
-        info_position: 'top-right',
       },
     ],
     
