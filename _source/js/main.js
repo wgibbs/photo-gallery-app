e87ca8044e55571d3833a533924aa5c28c4ef72e
Wesley Gibbs <wcgibbsey@gmail.com>
2021-04-25 16:52:55 -0400

    Component updates

--- _source/js/main.js
+++ _source/js/main.js
@@ -1,10 +1,75 @@
-
 //Components
-import GalleryPhoto from './components/GalleryPhoto.vue';
+var GalleryButton = {
+  props: [
+    'title',
+    'dataImgPath'
+  ],
+
+  data: function () {
+    return {
+      visible: 'visible',
+      hidden: 'hidden',
+      dataId: 'data-id',
+      galleryButtons: '.gallery__buttons',
+    }
+  },
+  
+  template: '\
+    <button class="gallery__button" :aria-label="title" @click="showPhoto">\
+      <img :src="dataImgPath" alt="Gallery Button Image" />\
+    </button>',
+  
+  methods: {
+    showPhoto: function(e) {
+      const currentId = e.target.parentElement.getAttribute(this.dataId);
+    
+      document.getElementById(currentId).classList.add(this.visible);
+      document.querySelector(this.galleryButtons).classList.add(this.hidden);
+    }
+  }
+};
+
+var GalleryPhoto = {
+  props: [
+    'title',
+  ],
+
+  data: function () {
+    return {
+      visible: 'visible',
+      hidden: 'hidden',
+      galleryPhoto: '.gallery__photo',
+      galleryButtons: '.gallery__buttons',
+    }
+  },
+  
+  template: '\
+    <div role="img" class="gallery__photo">\
+      <div class="gallery__photo-info">\
+        <h2 class="gallery__photo-title">{{ title }}</h2>\
+      </div>\
+      <button class="gallery__close-btn" aria-label="Close Photo" @click="closePhoto">&#x2715;</button>\
+    </div>',
+  
+  methods: {
+   closePhoto: function(e) {
+      const galleryPhotos = document.querySelectorAll(this.galleryPhoto);
+      
+      galleryPhotos.forEach(photo => {
+        photo.classList.remove(this.visible);
+      });
+      document.querySelector(this.galleryButtons).classList.remove(this.hidden);
+    },
+  }
+};
 
 //Main App
 var galleryApp = new Vue({
   el: '#galleryApp',
+  components: {
+    'gallery-button': GalleryButton,
+    'gallery-photo': GalleryPhoto,
+  },
   data: {
     galleryPhotos: [
       {
