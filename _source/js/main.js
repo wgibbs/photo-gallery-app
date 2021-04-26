e2b4a73a73ac7ccf86c24e534e76c9ed73adb8e4
Wesley Gibbs <wcgibbsey@gmail.com>
2021-04-26 09:17:00 -0400

    Component updates

--- _source/js/main.js
+++ _source/js/main.js
@@ -1,64 +1,110 @@
-//Components
-var GalleryButton = {
-  props: [
-    'title',
-    'dataImgPath'
-  ],
-
-  data: function () {
-    return {
-      visible: 'visible',
-      hidden: 'hidden',
-      dataId: 'data-id',
-      galleryButtons: '.gallery__buttons',
-    }
-  },
-  
-  template: '\
-    <button class="gallery__button" :aria-label="title" @click="showPhoto">\
-      <img :src="dataImgPath" alt="Gallery Button Image" />\
-    </button>',
-  
-  methods: {
-    showPhoto: function(e) {
-      const currentId = e.target.parentElement.getAttribute(this.dataId);
-    
-      document.getElementById(currentId).classList.add(this.visible);
-      document.querySelector(this.galleryButtons).classList.add(this.hidden);
-    }
-  }
+//Data
+var galleryData = {
+  galleryItems: [
+    {
+      title: 'Arches National Park, UT',
+      image_url: './img/arches-utah-1.jpg',
+    },
+    {
+      title: 'Squamish, BC',
+      image_url: './img/squamish-bc-2.jpg',
+    },
+    {
+      title: 'Bonneville Salt Flats, UT',
+      image_url: './img/bonneville-utah-1.jpg',
+    },
+    {
+      title: 'Garden of the Gods - Colorado Springs, CO',
+      image_url: './img/garden-of-the-gods-co-1.jpg',
+    },
+    {
+      title: 'Seattle, WA',
+      image_url: './img/seattle-wa-1.jpg',
+    },
+    {
+      title: 'Eugene, OR',
+      image_url: './img/eugene-oregon-1.jpg',
+    },
+    {
+      title: 'Cumberland Trail - Chattanooga, TN',
+      image_url: './img/chattanooga-1.jpg',
+    },
+    {
+      title: 'Arabia Mountain, GA',
+      image_url: './img/arabia-mt-1.jpg',
+    },
+    {
+      title: 'San Francisco, CA',
+      image_url: './img/san-fran-1.jpg',
+    },
+    {
+      title: 'Squamish, BC',
+      image_url: './img/squamish-bc-1.jpg',
+    },
+    {
+      title: 'Arches National Park, UT',
+      image_url: './img/arches-utah-2.jpg',
+    },
+    {
+      title: 'Seattle, WA',
+      image_url: './img/seattle-wa-2.jpg',
+    },
+    {
+      title: 'Seal Rock, OR',
+      image_url: './img/seal-rock-oregon-1.jpg',
+    },
+    {
+      title: 'Boulder Mountain Park - Boulder, CO',
+      image_url: './img/boulder-co-1.jpg',
+    },
+    {
+      title: 'Canby, OR',
+      image_url: './img/canby-or-1.jpg',
+    },
+    {
+      title: 'Willamette National Forest - Blue River, OR',
+      image_url: './img/willamette-national-forest-1.jpg',
+    },
+  ]
 };
 
-var GalleryPhoto = {
+//Components
+var galleryItem = {
   props: [
     'title',
+    'dataImgPath',
   ],
 
   data: function () {
     return {
-      visible: 'visible',
-      hidden: 'hidden',
-      galleryPhoto: '.gallery__photo',
-      galleryButtons: '.gallery__buttons',
+      galleryActive: 'gallery-active',
+      gallery: '.gallery__main',
+      isActive: false,
     }
   },
   
   template: '\
-    <div role="img" class="gallery__photo">\
-      <div class="gallery__photo-info">\
-        <h2 class="gallery__photo-title">{{ title }}</h2>\
+    <div class="gallery__item" :class="{ activeItem: isActive }">\
+      <button class="gallery__open-btn" :aria-label="title" @click="showPhoto">\
+        <img :src="dataImgPath" alt="Gallery Button Image" />\
+      </button>\
+       <div role="img" class="gallery__photo" :aria-hidden="[!isActive]" :aria-label="title" v-bind:style="{backgroundImage: \'url(\' + dataImgPath + \')\' }">\
+        <div class="gallery__photo-info">\
+          <h2 class="gallery__photo-title">{{ title }}</h2>\
+        </div>\
       </div>\
       <button class="gallery__close-btn" aria-label="Close Photo" @click="closePhoto">&#x2715;</button>\
     </div>',
   
   methods: {
-   closePhoto: function(e) {
-      const galleryPhotos = document.querySelectorAll(this.galleryPhoto);
-      
-      galleryPhotos.forEach(photo => {
-        photo.classList.remove(this.visible);
-      });
-      document.querySelector(this.galleryButtons).classList.remove(this.hidden);
+    showPhoto: function(e) {
+      document.querySelector(this.gallery).classList.add(this.galleryActive);
+      this.isActive = true;
+    },
+
+    closePhoto: function(e) {
+      this.isActive = false;
+      document.querySelector(this.gallery).classList.remove(this.galleryActive);
     },
   }
 };
@@ -66,91 +112,8 @@ var GalleryPhoto = {
 //Main App
 var galleryApp = new Vue({
   el: '#galleryApp',
+  data: galleryData,
   components: {
-    'gallery-button': GalleryButton,
-    'gallery-photo': GalleryPhoto,
+    'gallery-item': galleryItem,
   },
-  data: {
-    galleryPhotos: [
-      {
-        title: 'Arches National Park, UT',
-        image_url: './img/arches-utah-1.jpg',
-      },
-      {
-        title: 'Squamish, BC',
-        image_url: './img/squamish-bc-2.jpg',
-      },
-      {
-        title: 'Bonneville Salt Flats, UT',
-        image_url: './img/bonneville-utah-1.jpg',
-      },
-      {
-        title: 'Garden of the Gods - Colorado Springs, CO',
-        image_url: './img/garden-of-the-gods-co-1.jpg',
-      },
-      {
-        title: 'Seattle, WA',
-        image_url: './img/seattle-wa-1.jpg',
-      },
-      {
-        title: 'Eugene, OR',
-        image_url: './img/eugene-oregon-1.jpg',
-      },
-      {
-        title: 'Cumberland Trail - Chattanooga, TN',
-        image_url: './img/chattanooga-1.jpg',
-      },
-      {
-        title: 'Arabia Mountain, GA',
-        image_url: './img/arabia-mt-1.jpg',
-      },
-      {
-        title: 'San Francisco, CA',
-        image_url: './img/san-fran-1.jpg',
-      },
-      {
-        title: 'Squamish, BC',
-        image_url: './img/squamish-bc-1.jpg',
-      },
-      {
-        title: 'Arches National Park, UT',
-        image_url: './img/arches-utah-2.jpg',
-      },
-      {
-        title: 'Seattle, WA',
-        image_url: './img/seattle-wa-2.jpg',
-      },
-      {
-        title: 'Seal Rock, OR',
-        image_url: './img/seal-rock-oregon-1.jpg',
-      },
-      {
-        title: 'Boulder Mountain Park - Boulder, CO',
-        image_url: './img/boulder-co-1.jpg',
-      },
-      {
-        title: 'Canby, OR',
-        image_url: './img/canby-or-1.jpg',
-      },
-      {
-        title: 'Willamette National Forest - Blue River, OR',
-        image_url: './img/willamette-national-forest-1.jpg',
-      },
-    ],
-    visible: 'visible',
-    hidden: 'hidden',
-    dataId: 'data-id',
-    gallery: '.gallery',
-    galleryPhoto: '.gallery__photo',
-    galleryButtons: '.gallery__buttons',
-  },
-  
-  methods: {
-    showPhoto: function(e) {
-      const currentId = e.target.parentElement.getAttribute(this.dataId);
-      
-      document.getElementById(currentId).classList.add(this.visible);
-      document.querySelector(this.galleryButtons).classList.add(this.hidden);
-    }
-  }
 });
