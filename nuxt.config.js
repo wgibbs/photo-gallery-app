bfe198cddffd6939ea80ab41b48bda92f2f40596
Wesley Gibbs <wcgibbsey@gmail.com>
2021-06-15 20:26:58 -0400

    dist path update

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,8 +43,8 @@ export default {
   build: {
   },
 
-  //Router
-  router: {
-    base: 'https://wgibbs.github.io/photo-gallery-app/'
-  },
+  // //Router
+  // router: {
+  //   base: '/photo-gallery-app/'
+  // },
 }
