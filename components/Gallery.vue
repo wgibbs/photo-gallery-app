cccd8cab95da8b6f6a4ea0e2c9726470cc0ec1f5
Wesley Gibbs <wesleygibbs@Wesleys-Mac-mini.local>
2021-07-02 08:27:59 -0400

    UI updates

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -7,14 +7,13 @@
       <button
         :aria-label="'Open Expanded Photo of '+ galleryItem.title" 
         @click="activateGallery(index)"
-        class="overflow-hidden relative border border-black focus:outline-custom"
+        class="overflow-hidden focus:outline-custom relative border-2 md:border-4 border-transparent hover:border-black transition duration-500"
         :key="index" 
         v-for="(galleryItem, index) in galleryItems" 
       >
         <img 
           :src="galleryItem.image_url" 
           :alt="galleryItem.title + ' Button Image'"
-          class="transform transition duration-500 hover:scale-110" 
         />
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 sm:h-6 sm:w-6 absolute right-1 bottom-1 bg-black bg-opacity-70 rounded text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
