4abf714aca5322ff511e962fdc0fbce7c0eea77a
Wesley Gibbs <wesleygibbs@Wesleys-Mac-mini.local>
2021-07-02 08:30:00 -0400

    UI updates

--- _nuxt/static/1625226418/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fphoto-gallery-app\u002F_nuxt\u002Fstatic\u002F1625226418",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fphoto-gallery-app\u002F",assetsPath:"\u002Fphoto-gallery-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
