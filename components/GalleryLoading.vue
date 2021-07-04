2758c18ffa24033d2e76bdf7924a8d38f0f67639
Wesley Gibbs <wesleygibbs@Wesleys-Mac-mini.local>
2021-07-04 16:25:41 -0400

    formatting fix

--- components/GalleryLoading.vue
+++ components/GalleryLoading.vue
@@ -1,6 +1,7 @@
 <template>
   <div 
-    class="gallery__loading container px-4 overflow-hidden flex items-start justify-center mx-auto w-full max-w-full h-screen" v-show="!this.loadingStatus"
+    class="gallery__loading container px-4 overflow-hidden flex items-start justify-center mx-auto w-full max-w-full h-screen"
+    v-show="!this.loadingStatus"
   >
     <IconLoading />
   </div>
